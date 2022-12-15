0989f8a518a5b54ada22554dc01d3463c6a00215
Vinicius Caceres <vinicius.caceres@pmsma.local>
2022-12-15 17:27:19 -0300

    feat: add new post it

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function Home() {
 
         <section className=" mx-auto pt-40">
           <h3 className=" text-5xl text-center">Todos nossos sites incluem:</h3>
-          <ul className=" flex ">
+          <ul className=" flex flex-wrap justify-center items-center ">
             <li>
               <PostIt texto="Link para as suas redes sociais: Instagram, Linkedin, Facebook, Twitter." />{" "}
             </li>
@@ -118,40 +118,97 @@ export default function Home() {
               <PostIt texto=" Compatibilidade com Celular, Tablet e PC." />{" "}
             </li>
             <li>
-              {" "}
-              <PostIt texto="Segurança: certificado SSL gratuito." />
+              <PostIt texto="Botão para comunicação direta com o seu WhatsApp" />
             </li>
             <li>
-              {" "}
-              <PostIt texto="Botão para comunicação direta com o seu WhatsApp" />
+              <PostIt texto="Formulário de Contato" />
+            </li>
+            <li>
+              <PostIt texto="Segurança: certificado SSL gratuito." />
             </li>
           </ul>
         </section>
 
         <div>
-          <section>
-            <strong>Passo a Passo para criarmos seu WebSite:</strong>
-            <ul className=" flex">
-              <li className=" border border-black h-auto">
+          <section className=" flex flex-col justify-center">
+            <em className=" text-5xl text-center">
+              Passo a Passo para criarmos o WebSite certo para você:
+            </em>
+            <ul className=" flex justify-center items-center">
+              <li className=" border border-black p-5 w-60 h-72">
                 1º Passo: Você nos passa todos os detalhes que você deseja em
-                seu site: logo, imagens/fotos, sites que você achou bonito...
+                seu site: logo, imagens/fotos, sites modelo...
               </li>
-              <li className=" border border-black h-auto">
+              <li className=" border border-black p-5 w-60 h-72">
                 2º Passo: Enviamos a você um layout personalizado do seu site.
               </li>
-              <li className=" border border-black h-auto">
+              <li className=" border border-black p-5 w-60 h-72">
                 3º Passo: Aprovado o layout enviado, damos inicio ao
-                desenvolvimento.
+                desenvolvimento. Dependendo do layout, entre 5 a 7 dias úteis
               </li>
-              <li className=" border border-black h-auto">
-                Pronto! Agora você tem um belo site para atrair mais clientes!
+              <li className=" border border-black p-5 w-60 h-72">
+                Pronto! Agora você tem um site bonito, rápido e seguro para
+                captar mais clientes!
               </li>
             </ul>
           </section>
         </div>
 
-        <section>Conheça nosso planos:</section>
+        <section>
+          <h3 className=" text-5xl text-center">Preços e Planos</h3>
 
+          <div className=" flex flex-col ">
+            <button>Pagamento Anual</button>
+            <button>Pagamento Mensal</button>
+            <div>
+              <strong>Básico</strong>
+              <p>R$ 400,00</p>
+              <ul>
+                <li>Site Profissional Completo</li>
+                <li>Layout Responsivo</li>
+                <li>Hospedagem: Grátis por 06 meses</li>
+                <li>Automatização do WhatsApp: Não</li>
+                <li>Personalização: Limitada</li>
+                <li>Número de Páginas: 3</li>
+                <li>Postagem Mensais no Blog: 1 texto com 250 caracteres</li>
+              </ul>
+            </div>
+            <div>
+              <strong>Intermediário</strong>
+              <p>R$ 650,00</p>
+              <ul>
+                <li>Site Profissional Completo</li>
+                <li>Layout Responsivo</li>
+                <li>Hospedagem: Grátis por 06 meses</li>
+                <li>Automatização do WhatsApp: Sim</li>
+                <li>Personalização: Ilimitada</li>
+                <li>Número de Páginas: 6</li>
+                <li>
+                  Postagem Mensais no Blog: 2 textos com até 500 caracteres
+                </li>
+              </ul>
+            </div>
+            <div>
+              <strong>Premium</strong>
+              <p>R$ 950,00</p>
+              <ul>
+                <li>Site Profissional Completo</li>
+                <li>Layout Responsivo</li>
+                <li>Hospedagem: Grátis por 12 meses</li>
+                <li>Automatização do WhatsApp: Sim</li>
+                <li>Personalização: Ilimitada</li>
+                <li>Número de Páginas: Ilimitado</li>
+                <li>
+                  Postagem Mensais no Blog: 4 textos com até 1000 caracteres
+                </li>
+                <li>+ Guia para criação de vídeos no Youtube</li>
+                <li>+ Guia para criação de conteúdo textual</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        <section></section>
         <footer>
           <h2>Web para Advogados</h2>
         </footer>
